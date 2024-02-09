9117ca491c16fb7f7bbb631297969dbb96e2603a
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-09 11:48:09 +0000

    fixing static vs staticfiles problems

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    # Render the response and send it back!
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
