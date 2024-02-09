c65bc06119ee3ad29985e953aeaf01c935d0f963
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-09 12:53:35 +0000

    ch6 complete

--- rango/views.py
+++ rango/views.py
@@ -12,3 +12,17 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = dict()
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
