e7d75f6fe47c227288289e930041642f5289acf4
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-09 13:16:11 +0000

    ch7 completed

--- rango/models.py
+++ rango/models.py
@@ -18,8 +18,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
-
 class Page(models.Model):
     category = ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
