0bfd5522c0941591fd449275d8930e0fa1b393ef
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-09 10:34:08 +0000

    fixed ch3, finished ch4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for tango_with_django_project project.
+"""tango_with_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
