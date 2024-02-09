626deac1634023a819fa9ede54b3de72e0d6fa22
Masha Baidachna <realcowcitizen@gmail.com>
2024-02-09 11:12:46 +0000

    ch5 complete

--- rango/models.py
+++ rango/models.py
@@ -1,21 +1,26 @@
 from django.db import models
+from django.db.models import ForeignKey
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
+    # For special plural forms
     class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
-    
-    
+
+
 class Page(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-    
+
     def __str__(self):
         return self.title
     
